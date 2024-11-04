db2d6c9fe1b9c6da41ed470f5b17c0ab567c9321
Deepanshu-Mehta <146458164+Deepanshu-Mehta@users.noreply.github.com>
2024-11-04 21:23:47 +0530

    updated

--- Backend/app.js
+++ Backend/app.js
@@ -32,6 +32,6 @@ app.post('/generate-our-image-brotha', async (req, res)=> {
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT||3001
 
 app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
